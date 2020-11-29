77b08927bc2f7f92bfaa283442fd71696842951f
Justin <justin-reniers@hotmail.com>
2020-11-29 11:48:35 +0100

    Data class and BluetoothMessage class documentation

--- ex13_ev3rover_xtext/python/utils/bluetoothmessage.py
+++ ex13_ev3rover_xtext/python/utils/bluetoothmessage.py
@@ -3,12 +3,24 @@ class BluetoothMessage:
     A simple Bluetooth Message carrying a key and value
     """
     def __init__(self, key, value):
+        """
+        The initializer of the BluetoothMessage class.
+        :param key: The key that indexes the motors and sensors.
+        :param value: The value ascribed to the key.
+        """
         self.key = key
         self.value = value
 
     def __str__(self):
+        """
+        Overwrites the standard __str__ method with the key and value pair of the BluetoothMessage class.
+        """
         return '{0:}:{1:}'.format(self.key, self.value)
 
     @classmethod
     def read_message(cls, data):
+        """
+        Creates an object with the data in a BluetoothMessage, creating a new BluetoothMessage.
+        :param data:
+        """
         return cls(*data.split(':'))
--- ex13_ev3rover_xtext/python/utils/data.py
+++ ex13_ev3rover_xtext/python/utils/data.py
@@ -3,10 +3,21 @@ class Data:
     Database for sensory data
     """
     def __init__(self):
+        """
+        Initializer of the Data class. Gives it an empty dictionary to keep its data in.
+        """
         self.data = {}
     
     def put(self, btmsg):
+        """
+        Puts the contents of a BluetoothMessage in the dictionary of the Data object.
+        :param btmsg: The BluetoothMessage is put into the Data dictionary
+        """
         self.data[btmsg.key] = btmsg.value
 
     def __getattr__(self, item):
+        """
+        Obtains the value of the corresponding key (item) in the Data object its dictionary.
+        :param item: The key in the Data object from which we want to obtain the value.
+        """
         return self.data[item]
