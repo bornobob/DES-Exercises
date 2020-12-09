4c84359e580fdac7f66c42048a02c3356c43c9e3
Bob Ruiken <bobruiken@gmail.com>
2020-12-09 12:58:42 +0100

    Fixed up the collision action

--- ex13_ev3rover_xtext/python/actions/collisionaction.py
+++ ex13_ev3rover_xtext/python/actions/collisionaction.py
@@ -5,16 +5,28 @@ class CollisionAction(BaseAction):
     """
     The Collision Action turns around a bit when bumping into an object by checking the Touch Sensors.
     """
-    def __init__(self, priority, rotate_degrees=.4, rpm=30):
+    def __init__(self, priority, rotate_degrees=.4):
         super().__init__(priority)
         self.rotate_degrees = rotate_degrees
-        self.rpm = rpm
+        self.back_touch_is_pressed = False
+        self.rotate_right = False
 
     def check(self):
-        return self.robot.right_touch.is_pressed or self.robot.left_touch.is_pressed
+        if self.robot.database.ts_b:
+            self.back_touch_is_pressed = True
+        elif self.robot.database.ts_r:
+            self.rotate_right = False
+        elif self.robot.database.ts_l:
+            self.rotate_right = True
+        else:
+            return False
+        return True
 
     def _do_action(self):
-        if self.robot.left_touch.is_pressed:
-            self.robot.rotate_degrees(rotations=self.rotate_degrees, lock=self.lock, rpm=self.rpm)
-        elif self.robot.right_touch.is_pressed:
-            self.robot.rotate_degrees(rotations=self.rotate_degrees, lock=self.lock, rpm=-self.rpm)
+        print(self.robot.database.data)
+        if self.back_touch_is_pressed:
+            self.robot.rotate_degrees(rotations=self.rotate_degrees, reverse_before_continue=False, lock=self.lock)
+        elif self.rotate_right:
+            self.robot.rotate_degrees(rotations=self.rotate_degrees, lock=self.lock)
+        else:
+            self.robot.rotate_degrees(rotations=-self.rotate_degrees, lock=self.lock)
--- ex13_ev3rover_xtext/python/main.py
+++ ex13_ev3rover_xtext/python/main.py
@@ -1,4 +1,4 @@
-from actions import DriveAction, BorderAction
+from actions import DriveAction, BorderAction, CollisionAction
 from runner import Runner
 from ev3dev2.unit import STUD_MM
 from robot import Robot
@@ -18,7 +18,7 @@ sensor_map_master = {'tank_drive': MoveDifferential(OUTPUT_A, OUTPUT_D, EV3Educa
 
 def create_runner():
 	r = Robot(SensorMap(sensor_map_master), bluetooth=BluetoothMaster(MAC_ADDRESS, PORT))
-	mission_ToTheMoon = [DriveAction(priority=0), BorderAction(priority=1)]
+	mission_ToTheMoon = [DriveAction(priority=0), BorderAction(priority=2), CollisionAction(priority=1)]
 	Runner(r, mission_ToTheMoon).run()
 
 
