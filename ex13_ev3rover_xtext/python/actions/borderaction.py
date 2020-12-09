657711a6f718359f194be2b62932c2319a558430
Bob Ruiken <bobruiken@gmail.com>
2020-12-09 12:30:21 +0100

    Fixed up the borderaction for the big robot.

--- ex13_ev3rover_xtext/python/actions/borderaction.py
+++ ex13_ev3rover_xtext/python/actions/borderaction.py
@@ -9,10 +9,22 @@ class BorderAction(BaseAction):
     def __init__(self, priority, rotate_degrees=.3):
         super().__init__(priority)
         self.rotate_degrees = rotate_degrees
+        self.turn_right = False
 
     def check(self):
-        return self.robot.sensormap.cs_l.color == ColorSensor.COLOR_WHITE
+        if self.robot.sensormap.cs_l.color == ColorSensor.COLOR_WHITE:
+            self.turn_right = True
+        elif self.robot.sensormap.cs_r.color == ColorSensor.COLOR_WHITE:
+            self.turn_right = False
+        elif self.robot.sensormap.cs_m.color == ColorSensor.COLOR_WHITE:
+            self.turn_right = True
+        else:
+            return False
+        return True
 
     def _do_action(self):
         self.robot.sensormap.tank_drive.stop()
-        self.robot.rotate_degrees(self.rotate_degrees, lock=self.lock)
+        if self.turn_right:
+            self.robot.rotate_degrees(self.rotate_degrees, lock=self.lock)
+        else:
+            self.robot.rotate_degrees(-self.rotate_degrees, lock=self.lock)
