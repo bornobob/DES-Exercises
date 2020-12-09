2f16043217046fb21bfd395c9cb164bde99c99e7
Bob Ruiken <bobruiken@gmail.com>
2020-12-09 12:58:26 +0100

    Let database return None when the key is not found

--- ex13_ev3rover_xtext/python/utils/data.py
+++ ex13_ev3rover_xtext/python/utils/data.py
@@ -13,11 +13,14 @@ class Data:
         Puts the contents of a BluetoothMessage in the dictionary of the Data object.
         :param btmsg: The BluetoothMessage is put into the Data dictionary
         """
-        self.data[btmsg.key] = btmsg.value
+        self.data[btmsg.key] = float(btmsg.value)
 
     def __getattr__(self, item):
         """
         Obtains the value of the corresponding key (item) in the Data object its dictionary.
         :param item: The key in the Data object from which we want to obtain the value.
         """
-        return self.data[item]
+        try:
+            return self.data[item]
+        except KeyError:
+            return None
