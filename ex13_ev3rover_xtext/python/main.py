b199a3220145744c8bbe3f4f23e3b7615cc617e9
Bob Ruiken <bobruiken@gmail.com>
2021-01-18 14:49:21 +0100

    Created a main file with which you can run both separate file

--- ex13_ev3rover_xtext/python/main.py
+++ ex13_ev3rover_xtext/python/main.py
@@ -1,46 +1,17 @@
-from actions.measureaction import MeasureAction
-from actions import *
-from runner import Runner
-from ev3dev2.unit import STUD_MM
-from robot import Robot
-from ev3dev2.sensor.lego import UltrasonicSensor, ColorSensor
-from ev3dev2.wheel import EV3EducationSetTire
-from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_D, MoveDifferential, Motor
-from utils import SensorMap, BluetoothMaster
-from celebrations import *
-from mission import Mission
+import subprocess
+import time
 
 
-MAC_ADDRESS = '00:1a:7d:da:71:10'  # Mac address via which the Robots connect with bluetooth
-PORT = 4  # Port on which the Robots connect with bluetooth
-
-sensor_map_master = {'tank_drive': MoveDifferential(OUTPUT_A, OUTPUT_D, EV3EducationSetTire, 15 * STUD_MM),
-                     'measurement_motor': Motor(OUTPUT_B),
-                     'cs_l': ColorSensor('ev3-ports:in1'), 'cs_m': ColorSensor('ev3-ports:in2'),
-                     'cs_r': ColorSensor('ev3-ports:in3'), 'us_b': UltrasonicSensor('ev3-ports:in4')}
-
-def create_runner():
-	r = Robot(SensorMap(sensor_map_master), bluetooth=BluetoothMaster(MAC_ADDRESS, PORT))
-	mission_ToTheMoonAndBeyond = Mission(
-		[DriveAction(priority=0),
-		 PushRockAction(priority=3, number_of_rocks=1),
-		 BorderAction(priority=10),
-		 DontDrownAction(priority=5, lakes=[ColorSensor.COLOR_RED, ColorSensor.COLOR_BLUE, ColorSensor.COLOR_YELLOW])],
-		DanceCelebration())
-	mission_YeetRocks = Mission(
-		[DriveAction(priority=0),
-		 MeasureAction(priority=5, colors=[ColorSensor.COLOR_RED, ColorSensor.COLOR_BLUE]),
-		 BorderAction(priority=10),
-		 DontDrownAction(priority=3, lakes=[ColorSensor.COLOR_RED, ColorSensor.COLOR_BLUE, ColorSensor.COLOR_YELLOW])],
-		SpeakCelebration('Wee I\'ve never been happier in my life'))
-	mission_FindColours = Mission(
-		[DriveAction(priority=0),
-		 ColorDetAction(priority=5, colors=[ColorSensor.COLOR_RED, ColorSensor.COLOR_BLUE]),
-		 BorderAction(priority=10),
-		 DontDrownAction(priority=3, lakes=[ColorSensor.COLOR_RED, ColorSensor.COLOR_BLUE, ColorSensor.COLOR_YELLOW])],
-		DanceCelebration())
-	Runner(r, [mission_FindColours]).run()
+PYTHON_EXECUTABLE = 'python'
+BRICK_1_PY = 'main_main.py'
+BRICK_2_PY = 'main_slave.py'
 
 
-if __name__ == '__main__':
-	create_runner()
+if __name__ == "__main__":
+    proc1 = subprocess.Popen([PYTHON_EXECUTABLE, BRICK_1_PY])
+    time.sleep(1)
+    proc2 = subprocess.Popen([PYTHON_EXECUTABLE, BRICK_2_PY])
+
+    input('Press [enter] to stop the simulation')
+    proc2.kill()
+    proc1.kill()
---
+++ ex13_ev3rover_xtext/python/main_main.py
@@ -0,0 +1,46 @@
+from actions.measureaction import MeasureAction
+from actions import *
+from runner import Runner
+from ev3dev2.unit import STUD_MM
+from robot import Robot
+from ev3dev2.sensor.lego import UltrasonicSensor, ColorSensor
+from ev3dev2.wheel import EV3EducationSetTire
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_D, MoveDifferential, Motor
+from utils import SensorMap, BluetoothMaster
+from celebrations import *
+from mission import Mission
+
+
+MAC_ADDRESS = '00:1a:7d:da:71:10'  # Mac address via which the Robots connect with bluetooth
+PORT = 4  # Port on which the Robots connect with bluetooth
+
+sensor_map_master = {'tank_drive': MoveDifferential(OUTPUT_A, OUTPUT_D, EV3EducationSetTire, 15 * STUD_MM),
+                     'measurement_motor': Motor(OUTPUT_B),
+                     'cs_l': ColorSensor('ev3-ports:in1'), 'cs_m': ColorSensor('ev3-ports:in2'),
+                     'cs_r': ColorSensor('ev3-ports:in3'), 'us_b': UltrasonicSensor('ev3-ports:in4')}
+
+def create_runner():
+	r = Robot(SensorMap(sensor_map_master), bluetooth=BluetoothMaster(MAC_ADDRESS, PORT))
+	mission_ToTheMoonAndBeyond = Mission(
+		[DriveAction(priority=0),
+		 PushRockAction(priority=3, number_of_rocks=1),
+		 BorderAction(priority=10),
+		 DontDrownAction(priority=5, lakes=[ColorSensor.COLOR_RED, ColorSensor.COLOR_BLUE, ColorSensor.COLOR_YELLOW])],
+		DanceCelebration())
+	mission_YeetRocks = Mission(
+		[DriveAction(priority=0),
+		 MeasureAction(priority=5, colors=[ColorSensor.COLOR_RED, ColorSensor.COLOR_BLUE]),
+		 BorderAction(priority=10),
+		 DontDrownAction(priority=3, lakes=[ColorSensor.COLOR_RED, ColorSensor.COLOR_BLUE, ColorSensor.COLOR_YELLOW])],
+		SpeakCelebration('Wee I\'ve never been happier in my life'))
+	mission_FindColours = Mission(
+		[DriveAction(priority=0),
+		 ColorDetAction(priority=5, colors=[ColorSensor.COLOR_RED, ColorSensor.COLOR_BLUE]),
+		 BorderAction(priority=10),
+		 DontDrownAction(priority=3, lakes=[ColorSensor.COLOR_RED, ColorSensor.COLOR_BLUE, ColorSensor.COLOR_YELLOW])],
+		DanceCelebration())
+	Runner(r, [mission_FindColours]).run()
+
+
+if __name__ == '__main__':
+	create_runner()
